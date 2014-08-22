43fb1cafa626e4ff4ab4c972bb53212072aba452
Nate Brennand <natebrennand@gmail.com>
2014-08-21 18:07:26 -0700

    add ES support

--- es.go
+++ es.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/natebrennand/pg_array"
 )
+
+var (
+	esURL     string
+	esIndex   string
+	esType    = "courses"
+	batchSize = 200
+)
+
+func init() {
+	esIndex = getEnvVar("ES_INDEX")
+	esURL = fmt.Sprintf("http://%s:%s/",
+		getEnvVar("ES_HOST"),
+		getEnvVar("ES_PORT"),
+	)
+}
 
 type esData struct {
 	Course         string                  `db:"course"`     // EX: ZULU336
@@ -17,6 +35,45 @@ type esData struct {
 	Term           pg_array.SqlIntArray    `db:"term"`
 	CallNumber     pg_array.SqlIntArray    `db:"callnumber"`
 	Instructor     pg_array.SqlStringArray `db:"instructor"`
+}
+
+type esMetadata struct {
+	Index string `json:"es_index"`
+	Type  string `json:"_type"`
+	ID    string `json:"_id"` // will be the 'Course' attribute
+}
+
+type bulkItem struct {
+	MetaData esMetadata
+	Data     esData
+}
+
+type bulkInsert []bulkItem
+
+func (b bulkInsert) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	var encoder = json.NewEncoder(&buf)
+
+	for _, item := range b {
+		if err := encoder.Encode(item.MetaData); err != nil {
+			return []byte{}, fmt.Errorf("Failed to encode MetaData => %s", err.Error())
+		}
+		if err := encoder.Encode(item.Data); err != nil {
+			return []byte{}, fmt.Errorf("Failed to encode item data => %s", err.Error())
+		}
+	}
+	return buf.Bytes(), nil
+}
+
+func (d esData) NewBulkItem() bulkItem {
+	return bulkItem{
+		MetaData: esMetadata{
+			Index: esIndex,
+			Type:  esType,
+			ID:    d.Course,
+		},
+		Data: d,
+	}
 }
 
 var esQuery = `
@@ -47,6 +104,9 @@ func getESData(db *sql.DB) []esData {
 	}
 	defer rows.Close()
 
+	var batchBuffer = make([]bulkItem, batchSize)
+	var bufferIndex = 0
+
 	// process records
 	var data esData
 	for rows.Next() {
@@ -63,8 +123,49 @@ func getESData(db *sql.DB) []esData {
 		if err != nil {
 			log.Fatalf("Error while processing PG data => %s", err.Error())
 		}
-		fmt.Println(data)
+
+		batchBuffer[bufferIndex] = data.NewBulkItem()
+		bufferIndex++
+		if bufferIndex == batchSize {
+			insertEsData(bulkInsert(batchBuffer))
+			bufferIndex = 0
+		}
 	}
+	// insert remainder of buffer
+	insertEsData(bulkInsert(batchBuffer[0:bufferIndex]))
 
 	return esDataList
+}
+
+func deleteIndex() error {
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/%s", esURL, esIndex), nil)
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("Problem deleting ES index => %s", err.Error())
+	} else if resp.StatusCode/100 != 2 {
+		return fmt.Errorf("Problem deleting ES index => status code = %d", resp.StatusCode)
+	}
+	return nil
+}
+
+func insertEsData(data bulkInsert) error {
+	if len(data) == 0 { // don't insert if no data
+		return nil
+	}
+
+	var buf bytes.Buffer
+	var encoder = json.NewEncoder(&buf)
+	encoder.Encode(data)
+
+	client := http.Client{}
+	resp, err := client.Post(fmt.Sprintf("%s_bulk", esURL), "application/json", &buf)
+	if err != nil {
+		return fmt.Errorf("Failure stuffing data into ES => %s", err.Error())
+	} else if resp.StatusCode/100 != 2 {
+		return fmt.Errorf("Problem stuffing data into  ES => status code = %d", resp.StatusCode)
+	}
+
+	return nil
 }
---
+++ es_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/natebrennand/pg_array"
+)
+
+var (
+	testEsData = esData{
+		Course:         "test",
+		CourseFull:     "123",
+		CourseSubtitle: "test course subtitle",
+		CourseTitle:    "test",
+		Description:    "a course for testing",
+		Term: pg_array.SqlIntArray{
+			Data: []int64{1, 2, 3},
+		},
+		CallNumber: pg_array.SqlIntArray{
+			Data: []int64{4, 5, 6},
+		},
+		Instructor: pg_array.SqlStringArray{
+			Data: []string{"teacher1", "teacher2"},
+		},
+	}
+	testEsMetadata = esMetadata{
+		Index: "test",
+		Type:  "courses",
+		ID:    "123",
+	}
+	testBulkItem = bulkItem{
+		MetaData: testEsMetadata,
+		Data:     testEsData,
+	}
+	testBulkInsert  = bulkInsert([]bulkItem{testBulkItem})
+	testBulkInsert2 = bulkInsert([]bulkItem{testBulkItem, testBulkItem})
+)
+
+func TestBulkInsertMarshal(t *testing.T) {
+	jsonBytes, err := testBulkInsert.MarshalJSON()
+	if err != nil {
+		t.Errorf("Error encoding JSON => %s", err.Error())
+	}
+
+	buf := bytes.NewBuffer(jsonBytes)
+	if buf.String() != expectedJSON {
+		t.Error("ES JSON not encoded as expected.")
+	}
+}
+
+func TestBulkInsertMarshal2(t *testing.T) {
+	jsonBytes, err := testBulkInsert2.MarshalJSON()
+	if err != nil {
+		t.Errorf("Error encoding JSON => %s", err.Error())
+	}
+
+	buf := bytes.NewBuffer(jsonBytes)
+	if buf.String() != expectedJSON2 {
+		t.Error("ES JSON not encoded as expected.")
+	}
+}
+
+// Newlines are expected after each of the json segments
+//
+// Spec: http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-bulk.html
+var expectedJSON = `{"es_index":"test","_type":"courses","_id":"123"}
+{"Course":"test","CourseFull":"123","CourseSubtitle":"test course subtitle","CourseTitle":"test","Description":"a course for testing","Term":[1,2,3],"CallNumber":[4,5,6],"Instructor":["teacher1","teacher2"]}
+`
+var expectedJSON2 = `{"es_index":"test","_type":"courses","_id":"123"}
+{"Course":"test","CourseFull":"123","CourseSubtitle":"test course subtitle","CourseTitle":"test","Description":"a course for testing","Term":[1,2,3],"CallNumber":[4,5,6],"Instructor":["teacher1","teacher2"]}
+{"es_index":"test","_type":"courses","_id":"123"}
+{"Course":"test","CourseFull":"123","CourseSubtitle":"test course subtitle","CourseTitle":"test","Description":"a course for testing","Term":[1,2,3],"CallNumber":[4,5,6],"Instructor":["teacher1","teacher2"]}
+`
